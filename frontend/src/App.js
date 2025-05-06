3108a34d9792329ab12e4c476c348f0d6fb33966
charlesTTS <charles.ts.tu@gmail.com>
2025-05-06 12:40:14 +0800

    demo: dummy current user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { CurrentLoginUser } from './Components/CurrentLoginUser'
+import { CartContext } from './Components/CartContext'
 import NavBar from './Components/NavBar'
 import Home from "./Pages/Home"
-import { CartContext } from './Components/CartContext'
 import Product from './Pages/Product'
 import Search from './Pages/Search'
 import AdminPage from './Pages/Admin'
@@ -14,7 +15,15 @@ import Payment from './Pages/Payment'
 
 function App() {
   const [navHeight, setNavHeight] = useState(0);
-	const [cartItems, setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([])
+  const [currentUser, setCurrentUser] = useState({
+    userID: "currentUserID",
+    isAdmin: false,
+    userName: "currentUserName",
+    password: "currentUserPassword",
+    email: "currentUserEmail",
+    address: "currentUserAddress"
+  });
 
   useEffect(() => {
     const navBar = document.querySelector('nav');
@@ -26,34 +35,36 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <NavBar />
-			  <CartContext.Provider value={{ cartItems, setCartItems }}>
-          <div style={{ marginTop: `${navHeight}px`, overflowY: 'auto', overflowX: 'auto' }}>
-            <Routes >
-              <Route path="/" element={<Home />} />
+        <CurrentLoginUser.Provider value={{ currentUser, setCurrentUser }}>
+          <NavBar />
+          <CartContext.Provider value={{ cartItems, setCartItems }}>
+            <div style={{ marginTop: `${navHeight}px`, overflowY: 'auto', overflowX: 'auto' }}>
+              <Routes >
+                <Route path="/" element={<Home />} />
 
-              <Route path="/product" element={<Product />}>
-                <Route path=":id" element={<Product />} />
-              </Route>
+                <Route path="/product" element={<Product />}>
+                  <Route path=":id" element={<Product />} />
+                </Route>
 
-              <Route path="/admin" element={<AdminPage />} />
+                <Route path="/admin" element={<AdminPage />} />
 
-              <Route path="/profile" element={<UserProfilePage />} />
+                <Route path="/profile" element={<UserProfilePage />} />
 
-            <Route path="/reset-password" element={<ResetPasswordPage />} />
+                <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-            <Route path="/history" element={<PurchaseHistory />} />
-            
-            <Route path="/cart" element={<ShoppingCart />} />
+                <Route path="/history" element={<PurchaseHistory />} />
 
-            <Route path="/search" element={<Search />}>
-              <Route path=":keywords" element={<Search />} />
-            </Route>
+                <Route path="/cart" element={<ShoppingCart />} />
 
-              <Route path="*" element={<p>404 not found</p>} />
-            </Routes >
-          </div>
-        </CartContext.Provider>
+                <Route path="/search" element={<Search />}>
+                  <Route path=":keywords" element={<Search />} />
+                </Route>
+
+                <Route path="*" element={<p>404 not found</p>} />
+              </Routes >
+            </div>
+          </CartContext.Provider>
+        </CurrentLoginUser.Provider>
       </BrowserRouter>
     </div>
   );
--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,15 +1,30 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useContext, useState } from "react"
+import { CurrentLoginUser } from "./CurrentLoginUser"
 import styles from './NavBar.module.css';
 
 export default function NavBar() {
-    let [isAdmin, setIsAdmin] = useState(false)
     const [keywords, setKeywords] = useState('')
+    const { currentUser, setCurrentUser } = useContext(CurrentLoginUser)
 
     const logoutOnClick = () => {
-        setIsAdmin(!isAdmin)
-        console.log(isAdmin)
-    }
+        // implement logout logic here
+        // For example, clear user session, redirect to login page, etc.
+        console.log(`Is the user is an admin(before change): ${currentUser.isAdmin}`);
+        setCurrentUser({
+            userID: "currentUserID",
+            isAdmin: !currentUser.isAdmin,
+            userName: "currentUserName",
+            password: "currentUserPassword",
+            email: "currentUserEmail",
+            address: "currentUserAddress"
+        });
+    };
+
+    React.useEffect(() => {
+        console.log("The current user is: ", currentUser);
+        console.log(`Is the user is an admin: ${currentUser.isAdmin}`);
+    }, [currentUser]); // For logging, can be removed later
 
     const handleKeywordsChange = (event) => {
         setKeywords(event.target.value);
@@ -32,7 +47,7 @@ export default function NavBar() {
 
             {/* Middle Section */}
             <div className={styles.middle}>
-                <input type="text" placeholder="Search..." className={styles.searchBar} value={keywords} onChange={handleKeywordsChange} onKeyDown={handleKeyDown}/>
+                <input type="text" placeholder="Search..." className={styles.searchBar} value={keywords} onChange={handleKeywordsChange} onKeyDown={handleKeyDown} />
                 <Link to={'/search/' + keywords} className={styles.imgButton}>
                     <img src={process.env.PUBLIC_URL + '/Images/search.png'} alt="Search" className={styles.imgIcon} />
                 </Link>
@@ -40,7 +55,7 @@ export default function NavBar() {
 
             {/* Right Section */}
             <div className={styles.right}>
-                {isAdmin && <Link to="/admin" className={styles.navButton}>Admin Panel</Link>}
+                {currentUser.isAdmin && <Link to="/admin" className={styles.navButton}>Admin Panel</Link>}
                 <button onClick={logoutOnClick} className={styles.navButton}>Logout</button>
                 <Link to="/cart" className={styles.imgButton}>
                     <img src={process.env.PUBLIC_URL + '/Images/shoppingCart.png'} alt="Shopping Cart" className={styles.imgIcon} />
