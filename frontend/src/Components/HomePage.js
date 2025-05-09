579a26c2064bc50df4f61a033b3a75c9250593c3
charlesTTS <charles.ts.tu@gmail.com>
2025-05-09 08:27:43 +0800

    add: "Hello {userName}!"

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react"
+import { useContext, useState, useEffect } from "react"
+import { CurrentLoginUser } from "./CurrentLoginUser";
 import ProductCell from "./ProductCell"
 import style from "./HomePage.module.css"
 
@@ -6,6 +7,8 @@ export default function Home(props) {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [isHovered, setIsHovered] = useState(false)
     const [products, setProducts] = useState()
+    const { currentUser, setCurrentUser } = useContext(CurrentLoginUser);
+
 
     const fetchProduct = async () => {
         try {
@@ -52,19 +55,19 @@ export default function Home(props) {
 
     return (
         <div>
-            <h1 className={style.HomeName} >Welcome to BuyMe</h1>
+            <h1 className={style.HomeName} >Hello {currentUser.userName}! Welcome to BuyMe!</h1>
             <div
                 className={style.carousel}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                <button className={style.carouselButton} onClick={prevSlide}>
+                <button className={style.carouselButton} key="prevSlideBtn" onClick={prevSlide}>
                     &#9664;
                 </button>
-                <div className={style.carouselTrack}>
+                <div className={style.carouselTrack} key="carousel" >
                     <ProductCell productJSON={products[currentIndex]} />
                 </div>
-                <button className={style.carouselButton} onClick={nextSlide}>
+                <button className={style.carouselButton} key="nextSlideBtn" onClick={nextSlide}>
                     &#9654;
                 </button>
             </div>
